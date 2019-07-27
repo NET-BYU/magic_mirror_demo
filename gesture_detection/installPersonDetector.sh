2593673ab20c83074d350737e450b4f1aa76bff3
Joseph Miera <jmiera@byu.edu>
2019-07-26 21:13:01 -0600

    Added rudimentary script for reading the distance sensor, and added an empty install script

---
+++ gesture_detection/installPersonDetector.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script will install the person detector and create a systemd service for it to run at startup
+echo Hello World!
---
+++ gesture_detection/personDetector.py
@@ -0,0 +1,18 @@
+from gpiozero import DistanceSensor
+import time
+
+# NOTE - This code is not finished and still needs to (1) Simulate keypresses (2) Read from a config.json file to allow configuration
+
+def activate_mirror():
+	print("Mirror Activated")
+
+def deactivate_mirror():
+	print("Mirror Deactivated")
+
+ultrasonic = DistanceSensor(echo=17, trigger=4, max_distance=2, threshold_distance=1)
+ultrasonic.when_in_range = activate_mirror
+ultrasonic.when_out_of_range = deactivate_mirror
+
+while True:
+	print(ultrasonic.distance)
+	time.sleep(1)
