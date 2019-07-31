202c9ce9cfa644dbb191227f05981fb39b2bcc1f
Joseph Miera <jmiera@byu.edu>
2019-07-31 13:28:57 -0600

    Added example config file, implemented key presses

--- gesture_detection/personDetector.py
+++ gesture_detection/personDetector.py
@@ -1,19 +1,37 @@
 from gpiozero import DistanceSensor
 import time
-import pynput
+from pynput.keyboard import Key, Controller
 
 # TODO - (1) Simulate keypresses (2) Read from a config.json file to allow configuration
 
-def activate_mirror():
-	print("Mirror Activated")
+class PersonDetector:
+	ultrasonic = None
+	keyboard = None
+	activate_key = None
+	deactivate_key = None
+	def __init__(self, echo_pin=17, trigger_pin=4, max_dist=2, threshold_dist=1, activate_key=' ', deactivate_key='h'):
+		self.ultrasonic = DistanceSensor(echo=echo_pin, trigger=trigger_pin, max_distance=max_dist, threshold_distance=threshold_dist)
+		self.ultrasonic.when_in_range = self.__activate_mirror
+		self.ultrasonic.when_out_of_range = self.__deactivate_mirror
+		self.keyboard = Controller()
+		self.activate_key = activate_key
+		self.deactivate_key = deactivate_key
+	def __activate_mirror(self):
+		print("Mirror Activated")
+		self.keyboard.press(self.activate_key)
+		self.keyboard.release(self.activate_key)
+	def __deactivate_mirror(self):
+		print("Mirror Deactivated")
+		self.keyboard.press(self.deactivate_key)
+		self.keyboard.release(self.deactivate_key)
+	@classmethod
+	def init_from_json(jsonFileName="config.json"):
+		print("Initializing from " + jsonFileName)
+		
 
-def deactivate_mirror():
-	print("Mirror Deactivated")
 
-ultrasonic = DistanceSensor(echo=17, trigger=4, max_distance=2, threshold_distance=1)
-ultrasonic.when_in_range = activate_mirror
-ultrasonic.when_out_of_range = deactivate_mirror
+pd = PersonDetector()
 
-while True:
-	print(ultrasonic.distance)
-	time.sleep(1)
+# while True:
+# 	print(ultrasonic.distance)
+#	time.sleep(1)
