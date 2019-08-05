48325bcea2477bf1bf1eee7f400f66e006910a0c
Maximilian Warner <maximilianwarner1517@gmail.com>
2019-08-05 17:05:29 -0600

    updated quotes list

--- messages/quote/quotes_list_gen.py
+++ messages/quote/quotes_list_gen.py
@@ -7,28 +7,6 @@
 
 import json
 
-# quotes = {}
-# quotes['quotes'] = []
-# quotes['quotes'].append({
-#     'author': 'Karl G. Maeser',
-#     'quote_text': '"I am honest, etc."',
-# })
-# quotes['quote'].append({
-#     'author': 'Colin Powell',
-#     'quote_text': '"There are no secrets to success. It is the result of preparation, hard work and learning from failure."',
-# })
-# quotes['quote'].append({
-#     'author': 'Russel M. Nelson',
-#     'quote_text': '"We can do better and be better."',
-# })
-# quotes['quote'].append({
-#     'author': 'Winston Churchill',
-#     'quote_text': '"Success consists of going from failure to failure without loss of enthusiasm."',
-# })
-#
-# with open('quotes_list.txt', 'w') as outfile:
-#     json.dump(quotes, outfile, indent=4)
-
 
 quotes = []
 quote = {
