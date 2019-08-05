c81f9ec5b0d092b013f9471c0542488f422b7846
Maximilian Warner <maximilianwarner1517@gmail.com>
2019-08-05 17:02:55 -0600

    Updated quote list

--- messages/quote/quotes_list_gen.py
+++ messages/quote/quotes_list_gen.py
@@ -43,7 +43,12 @@ quote = {
 quotes.append(quote)
 quote = {
     'author': 'Russel M. Nelson',
-    'quote_text': '"We can do better and be better"',
+    'quote_text': '"We can do better and be better."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Billy Connolly',
+    'quote_text': '"Before you judge a man, walk a mile in his shoes. After that who cares?… He’s a mile away and you’ve got his shoes!"',
 }
 quotes.append(quote)
 quote = {
@@ -61,5 +66,66 @@ quote = {
     'quote_text': '"To educate a person in the mind but not in morals is to educate a menace to society."',
 }
 quotes.append(quote)
+quote = {
+    'author': 'Abraham Lincoln',
+    'quote_text': '"Better to remain silent and be thought a fool than to speak out and remove all doubt."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'C. S. Lewis',
+    'quote_text': '"True humility is not thinking less of yourself; it is thinking of yourself less."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Andrew Carnegie',
+    'quote_text': '"There is little success where there is little laughter"',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Socrates',
+    'quote_text': '"Education is the kindling of a flame, not the filling of a vessel."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Helen Keller',
+    'quote_text': '"The best and most beautiful things in this world cannot be seen or even heard but must be felt with the heart.',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Dale G. Renlund',
+    'quote_text': '"When God directs us to do one thing, He often has many purposes in mind."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'J. R. R. Tolkien',
+    'quote_text': '"Not all those who wander are lost."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Dalai Lama',
+    'quote_text': '"If you think you are too small to make a difference, try sleeping with a mosquito."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Massimo De Feo',
+    'quote_text': '"If you are struggling to find the strength to forgive, don\'t think of what others have done to you, but think of what the Lord has done for you."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Kevin J. Worthen',
+    'quote_text': '"You are more capable, more talented, & more faith-filled than you realize.  More important, you are more loved by God than you realize."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Tanner Gaskin',
+    'quote_text': '"That\'s awkward."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'C. S. Lewis',
+    'quote_text': '"I believe in Christianity as I believe that the sun has risen: not only because I see it, but because by it I see everything else."',
+}
+quotes.append(quote)
+
 with open('quotes_list.txt', 'w') as outfile:
     json.dump(quotes, outfile, indent=4)
