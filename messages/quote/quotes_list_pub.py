06ccef8ac51e30dcbfdef46f4e21be281828ab01
Maximilian Warner <maximilianwarner1517@gmail.com>
2019-08-05 12:18:29 -0600

    publishes quote from list in json format

--- messages/quote/quotes_list_pub.py
+++ messages/quote/quotes_list_pub.py
@@ -8,19 +8,19 @@ import paho.mqtt.publish as publish
 import json
 import time
 
-while True:
-    with open('quotes_list.txt') as json_file:
-        quotes = json.load(json_file)
-        for q in quotes['quote']:
-            # publish quote to MQTT broker
-            publish.single(
-                "immerse/quote",
-                q,
-                hostname="postman.cloudmqtt.com",
-                port=27408,
-                retain=True,
-                qos=1,
-                auth={"username": "messages", "password": "yMk7upKt2dcGEao3u2uxvXC4KnQRL224"},
-                tls={"ca_certs": "/home/maw276/magic_mirror_demo/messages/quote/ca.crt"},
-            )
-        time.sleep(60)
+with open('quotes_list.txt') as json_file:
+    quotes = json.load(json_file)
+    for quote in quotes:
+        # print(quote)
+        # publish quote to MQTT broker
+        publish.single(
+            "immerse/quote",
+            quote,
+            hostname="postman.cloudmqtt.com",
+            port=27408,
+            retain=True,
+            qos=1,
+            auth={"username": "messages", "password": "yMk7upKt2dcGEao3u2uxvXC4KnQRL224"},
+            tls={"ca_certs": "./ca.crt"},
+        )
+        time.sleep(1)
