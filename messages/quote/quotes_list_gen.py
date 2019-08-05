de8ce207cbbe1edb8d055adb1e256446b543b7f4
Maximilian Warner <maximilianwarner1517@gmail.com>
2019-08-05 12:19:59 -0600

    script to generate quotes list

--- messages/quote/quotes_list_gen.py
+++ messages/quote/quotes_list_gen.py
@@ -7,24 +7,59 @@
 
 import json
 
-quotes = {}
-quotes['quote'] = []
-quotes['quote'].append({
+# quotes = {}
+# quotes['quotes'] = []
+# quotes['quotes'].append({
+#     'author': 'Karl G. Maeser',
+#     'quote_text': '"I am honest, etc."',
+# })
+# quotes['quote'].append({
+#     'author': 'Colin Powell',
+#     'quote_text': '"There are no secrets to success. It is the result of preparation, hard work and learning from failure."',
+# })
+# quotes['quote'].append({
+#     'author': 'Russel M. Nelson',
+#     'quote_text': '"We can do better and be better."',
+# })
+# quotes['quote'].append({
+#     'author': 'Winston Churchill',
+#     'quote_text': '"Success consists of going from failure to failure without loss of enthusiasm."',
+# })
+#
+# with open('quotes_list.txt', 'w') as outfile:
+#     json.dump(quotes, outfile, indent=4)
+
+
+quotes = []
+quote = {
     'author': 'Karl G. Maeser',
-    'quote_text': '"I\'m honest, etc."',
-})
-quotes['quote'].append({
+    'quote_text': '"I am honest, etc."',
+}
+quotes.append(quote)
+quote = {
     'author': 'Colin Powell',
-    'quote_text': '“There are no secrets to success. It is the result of preparation, hard work and learning from failure.”',
-})
-quotes['quote'].append({
+    'quote_text': '"There are no secrets to success. It is the result of preparation, hard work and learning from failure."',
+}
+quotes.append(quote)
+quote = {
     'author': 'Russel M. Nelson',
-    'quote_text': '"We can do better and be better."',
-})
-quotes['quote'].append({
+    'quote_text': '"We can do better and be better"',
+}
+quotes.append(quote)
+quote = {
     'author': 'Winston Churchill',
     'quote_text': '"Success consists of going from failure to failure without loss of enthusiasm."',
-})
-
+}
+quotes.append(quote)
+quote = {
+    'author': 'Maya Angelou',
+    'quote_text': '"My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Theodore Roosevelt',
+    'quote_text': '"To educate a person in the mind but not in morals is to educate a menace to society."',
+}
+quotes.append(quote)
 with open('quotes_list.txt', 'w') as outfile:
-    json.dump(quotes, outfile)
+    json.dump(quotes, outfile, indent=4)
