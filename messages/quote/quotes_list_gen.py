12e16b1218af3b84572bd891de5a879dfbbc3333
Maximilian Warner <maximilianwarner1517@gmail.com>
2019-08-06 11:32:51 -0600

    Updated quotes list

--- messages/quote/quotes_list_gen.py
+++ messages/quote/quotes_list_gen.py
@@ -11,7 +11,12 @@ import json
 quotes = []
 quote = {
     'author': 'Karl G. Maeser',
-    'quote_text': '"I am honest, etc."',
+    'quote_text': '"Be yourself, but always your better self."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Thomas S. Monson',
+    'quote_text': '"Choose your love, Love your choice."',
 }
 quotes.append(quote)
 quote = {
@@ -25,8 +30,13 @@ quote = {
 }
 quotes.append(quote)
 quote = {
+    'author': 'Mark Twain',
+    'quote_text': '"The two most important days in your life are the day you are born and the day you find out why."',
+}
+quotes.append(quote)
+quote = {
     'author': 'Billy Connolly',
-    'quote_text': '"Before you judge a man, walk a mile in his shoes. After that who cares?… He’s a mile away and you’ve got his shoes!"',
+    'quote_text': '"Before you judge a man, walk a mile in his shoes. After that who cares? He\'s a mile away and you\'ve got his shoes!"',
 }
 quotes.append(quote)
 quote = {
@@ -40,8 +50,18 @@ quote = {
 }
 quotes.append(quote)
 quote = {
+    'author': 'Jeffrey R. Holland',
+    'quote_text': '"If for a while the harder you try, the harder it gets, take heart. So it has been with the best people who ever lived."',
+}
+quotes.append(quote)
+quote = {
     'author': 'Theodore Roosevelt',
     'quote_text': '"To educate a person in the mind but not in morals is to educate a menace to society."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Thomas S. Monson',
+    'quote_text': '"May we ever choose the harder right instead of the easier wrong."',
 }
 quotes.append(quote)
 quote = {
@@ -50,18 +70,43 @@ quote = {
 }
 quotes.append(quote)
 quote = {
+    'author': 'Russel M. Nelson',
+    'quote_text': '"Stellar spirits are often housed in imperfect bodies."',
+}
+quotes.append(quote)
+quote = {
     'author': 'C. S. Lewis',
     'quote_text': '"True humility is not thinking less of yourself; it is thinking of yourself less."',
 }
 quotes.append(quote)
 quote = {
+    'author': 'Sheri L. Dew',
+    'quote_text': '"There is one thing the power of God and the power of Satan have in common: Neither can influence us unless we allow them to."',
+}
+quotes.append(quote)
+quote = {
     'author': 'Andrew Carnegie',
-    'quote_text': '"There is little success where there is little laughter"',
+    'quote_text': '"There is little success where there is little laughter."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Laurence J. Peter',
+    'quote_text': '"Originality is the fine art of remembering what you hear but forgetting where you heard it."',
 }
 quotes.append(quote)
 quote = {
     'author': 'Socrates',
     'quote_text': '"Education is the kindling of a flame, not the filling of a vessel."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Jeffrey R. Holland',
+    'quote_text': '"Some blessings come soon, some come late, and some don\'t come until heaven; but for those who embrace the gospel of Jesus Christ, they come."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Thomas S. Monson',
+    'quote_text': '"Faith and doubt cannot exist in the same mind at the same time, for one will dispel the other."',
 }
 quotes.append(quote)
 quote = {
@@ -80,8 +125,28 @@ quote = {
 }
 quotes.append(quote)
 quote = {
+    'author': 'Karl G. Maeser',
+    'quote_text': '"He who cheats others is a knave, but he who cheats himself is a fool."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Russel M. Nelson',
+    'quote_text': '"Education is the difference between wishing you could help others and being able to help them."',
+}
+quotes.append(quote)
+quote = {
     'author': 'Dalai Lama',
     'quote_text': '"If you think you are too small to make a difference, try sleeping with a mosquito."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Mark Twain',
+    'quote_text': '"The secret of getting ahead is getting started."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Jeffrey R. Holland',
+    'quote_text': '"No misfortune is so bad that whining about it won\'t make it worse."',
 }
 quotes.append(quote)
 quote = {
@@ -95,13 +160,23 @@ quote = {
 }
 quotes.append(quote)
 quote = {
-    'author': 'Tanner Gaskin',
-    'quote_text': '"That\'s awkward."',
+    'author': 'Sheri L. Dew',
+    'quote_text': '"None of us come to this earth to gain our worth; we brought it with us."',
 }
 quotes.append(quote)
 quote = {
     'author': 'C. S. Lewis',
     'quote_text': '"I believe in Christianity as I believe that the sun has risen: not only because I see it, but because by it I see everything else."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Russel M. Nelson',
+    'quote_text': '"Truth is truth! It is not divisible, and any part of it cannot be set aside."',
+}
+quotes.append(quote)
+quote = {
+    'author': 'Thomas S. Monson',
+    'quote_text': '"Whether it is the best of times or the worst of times, He is with us. He has promised that this will never change."',
 }
 quotes.append(quote)
 
