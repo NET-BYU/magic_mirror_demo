472999fc8bc8af946b76518cd5196531f646602a
Maximilian Warner <maw276@immerse-iot.byu.edu>
2019-08-07 16:01:41 -0600

    Modified publish rate to match cronjob

--- messages/quote/quotes_list_pub.py
+++ messages/quote/quotes_list_pub.py
@@ -6,22 +6,24 @@
 ###########################
 import paho.mqtt.publish as publish
 import json
+import random
 import time
-while True:
-    with open('quotes_list.txt') as json_file:
-        quotes = json.load(json_file)
-        for quote in quotes:
-            # format string to json 
-            theQuote = "{}".format(quote)
-            # publish quote to MQTT broker
-            publish.single(
-                "immerse/quote",
-                theQuote,
-                hostname="postman.cloudmqtt.com",
-                port=27408,
-                retain=True,
-                qos=1,
-                auth={"username": "messages", "password": "yMk7upKt2dcGEao3u2uxvXC4KnQRL224"},
-                tls={"ca_certs": "./ca.crt"},
-            )
-            time.sleep(60)
+
+with open('/home/maw276/magic_mirror_demo/messages/quote/quotes_list.txt') as json_file:
+   quotes = json.load(json_file)
+   random_quote_index = random.randint(0, len(quotes) - 1)
+
+   # format string to json 
+   theQuote = json.dumps(quotes[random_quote_index])
+
+   # publish quote to MQTT broker
+   publish.single(
+       "immerse/quote",
+       theQuote,
+       hostname="postman.cloudmqtt.com",
+       port=27408,
+       retain=True,
+       qos=1,
+       auth={"username": "messages", "password": "yMk7upKt2dcGEao3u2uxvXC4KnQRL224"},
+       tls={"ca_certs": "/home/maw276/magic_mirror_demo/messages/quote/ca.crt"},
+   )
